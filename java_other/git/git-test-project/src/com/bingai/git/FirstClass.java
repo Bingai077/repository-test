35c6554cb0cdf82278a5a95794fa477d21e8ff50
bingai <492490375@qq.com>
2020-01-04 18:41:02 +0800

    把本地工程添加到git

---
+++ java_other/git/git-test-project/src/com/bingai/git/FirstClass.java
@@ -0,0 +1,26 @@
+package com.bingai.git;
+
+/**
+ * @author bingai
+ * @create 2020-01-04 18:30
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+
+}
