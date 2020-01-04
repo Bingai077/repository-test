4a75b2fdf079713c8636bd3b1b2e97c816d2d252
bingai <492490375@qq.com>
2020-01-04 18:54:01 +0800

    修改firstClass

--- java_other/git/git-test-project/src/com/bingai/git/FirstClass.java
+++ java_other/git/git-test-project/src/com/bingai/git/FirstClass.java
@@ -21,11 +21,13 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+        //hello test
     }
 
     public void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 
 }
